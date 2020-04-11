7f1eed1764ee27528d6bb2a87265b13060ce5ff2
youngkin <rich.youngkin@gmail.com>
2020-04-11 15:59:49 -0600

    add initial version of build script +semver: patch

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pre() {
+    go vet ./...
+    go fmt ./...
+    golint ./...
+    go test -race ./...
+}
+
+build() {
+    cd src/cmd/todod
+    go build
+    cd -
+}
+
+buildARM() {
+    cd src/cmd/todod
+    env GOOS=linux GOARCH=arm GOARM=7 go build
+    cd -
+}
+
+buildImage() {
+    doc
+}
+
+if [ $1 = "pre" ] 
+then
+    pre
+elif [ $1 = "build" ] 
+then
+    build
+elif [ $1 = "buildARM" ] 
+then
+    buildARM
+else
+    echo "usage:"
+    echo "  build.sh [pre | build | buildARM | tag | all]"
+    exit 1
+fi
